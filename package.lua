9a567e0e364d5999fd974a617eb614259577b767
Dennis Schridde <dennis.schridde@uni-heidelberg.de>
2016-08-16 19:25:53 +0200

    Bump version to 0.0.2

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "urzds/date",
-  version = "0.0.1",
+  version = "0.0.2",
   license = "Apache 2",
   homepage = "https://github.com/urzds/luvit-date",
   description = "Utility functions to parse dates",
