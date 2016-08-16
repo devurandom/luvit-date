383aefef26581c4a79257688c58575a89380d03b
Dennis Schridde <dennis.schridde@uni-heidelberg.de>
2016-08-16 19:23:28 +0200

    Bump to luvit/require@2.0.3 which fixes the luacov codefromstrings issue
    Also lock all dependencies to specific versions.
    
    Related: luvit/luvit#843

--- package.lua
+++ package.lua
@@ -6,8 +6,8 @@ return {
   description = "Utility functions to parse dates",
   tags = {"date"},
   dependencies = {
-    "luvit/require",
-    "luvit/tap",
-    "luvit/deep-equal",
+    "luvit/require@2.0.3",
+    "luvit/tap@0.1.1",
+    "luvit/deep-equal@0.1.2",
   }
 }
